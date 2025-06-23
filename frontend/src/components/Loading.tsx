09bd86336ec91ac2065ed3865df8647f737e1de3
Roman13-k <romank050607@gmail.com>
2025-06-23 22:25:09 +0300

    loading refactor and remove module.css

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 
 export function Loading() {
+  const delays = [0.6, 0.75, 0.9, 1.05, 1.2, 1.35, 1.5, 1.64];
+
   return (
-    <div className={styles.FountainG}>
-      <div className={`${styles.fountainG_1} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_2} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_3} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_4} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_5} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_6} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_7} ${styles.fountainG}`}></div>
-      <div className={`${styles.fountainG_8} ${styles.fountainG}`}></div>
+    <div className='flex gap-[5px] justify-center items-center w-[570px] h-[30px] mx-auto'>
+      {delays.map((delay, index) => (
+        <div
+          key={index}
+          className='w-[30px] h-[30px] rounded-full'
+          style={{
+            backgroundColor: "#d43814",
+            animation: `bounce-custom 1.5s ${delay}s infinite normal`,
+          }}></div>
+      ))}
+      <style jsx>{`
+        @keyframes bounce-custom {
+          0% {
+            transform: scale(1);
+            background-color: #d43814;
+          }
+          100% {
+            transform: scale(0.3);
+            background-color: #ffffff;
+          }
+        }
+      `}</style>
     </div>
   );
 }
