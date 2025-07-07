84dc9ee06abb97e94337218d1425d9148b189db4
romaperec <cpser621@gmail.com>
2025-07-07 17:00:13 +0300

    fix read env

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,9 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseConfig(BaseSettings):
     DB_URL: str = "postgresql+asyncpg://name:pass@localhost:5432/onechapter"
     DB_ECHO: bool = False
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 database_config = DatabaseConfig()
@@ -12,6 +14,8 @@ database_config = DatabaseConfig()
 class JWTConfig(BaseSettings):
     SECRET_KEY: str = "p0iy3I05KmNOh8Zjk2eTlVcrccX2VSR-Mfh-BMARw-jV1OpU3f7UDNoyZNae2DyWj0446eeuDi1edHyCPF43PQ"  # It's example
     ALGORITHM: str = "HS256"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 jwt_config = JWTConfig()
