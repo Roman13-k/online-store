621ca72ed1db3bb96592faa2370f259b89dffaaf
romaperec <cpser621@gmail.com>
2025-07-19 04:39:10 +0300

    Fix auth with oauth2

--- backend/src/utils/jwt.py
+++ backend/src/utils/jwt.py
@@ -1,6 +1,7 @@
 import jwt
 from jwt.exceptions import InvalidTokenError
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 
 from sqlalchemy import select
@@ -9,6 +10,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.models import BuyerModel, SellerModel
 from src.database.database import get_session
 from src.config import jwt_config
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/buyer")
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -24,13 +28,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_buyer(token: str, db: AsyncSession = Depends(get_session)):
-    if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token not found",
-        )
-
+async def get_current_buyer(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_session)):
     try:
         payload = jwt.decode(
             token, jwt_config.SECRET_KEY, algorithms=[jwt_config.ALGORITHM]
@@ -55,13 +53,7 @@ async def get_current_buyer(token: str, db: AsyncSession = Depends(get_session))
         )
 
 
-async def get_current_seller(token: str, db: AsyncSession = Depends(get_session)):
-    if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token not found",
-        )
-
+async def get_current_seller(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_session)):
     try:
         payload = jwt.decode(
             token, jwt_config.SECRET_KEY, algorithms=[jwt_config.ALGORITHM]
