db377704be2d3162d85fc5a6360e813bb2e26114
romaperec <cpser621@gmail.com>
2025-07-19 04:43:39 +0300

    Delete save one image function

--- backend/src/utils/images.py
+++ backend/src/utils/images.py
@@ -10,20 +10,6 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 ALLOW_EXTENSIONS = {"image/jpeg", "image/png", "video/mp4"}
 
 
-def save_image(image: UploadFile) -> str:
-    if image.content_type not in ALLOW_EXTENSIONS:
-        raise HTTPException(status_code=400, detail="File is not support")
-
-    file_ext = image.filename.split(".")[-1]
-    filename = f"{uuid.uuid4()}.{file_ext}"
-    file_path = os.path.join(UPLOAD_DIR, filename)
-
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(image.file, buffer)
-
-    return filename
-
-
 def save_multiple_images(images: List[UploadFile]) -> List[str]:
     saved_filenames = []
 
