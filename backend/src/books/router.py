a59bae7087f7448a6601369af97941eecadc85c1
romaperec <cpser621@gmail.com>
2025-08-07 13:48:50 +0300

    fix structure in router

--- backend/src/books/router.py
+++ backend/src/books/router.py
@@ -10,13 +10,11 @@ from .service import create_book, get_book_by_category, get_book_by_id
 router = APIRouter(prefix="/book", tags=["Books 📕"])
 
 
-@router.post("/add")
-async def add_book(
-    data: BookSchema,
-    token=Depends(oauth2_scheme),
-    db: AsyncSession = Depends(get_session),
+@router.get("/")
+async def get_book_with_category(
+    category: str, db: AsyncSession = Depends(get_session)
 ):
-    return await create_book(data=data, token=token, db=db)
+    return await get_book_by_category(category=category, db=db)
 
 
 @router.get("/{id}")
@@ -24,8 +22,10 @@ async def get_book_with_id(id: int, db: AsyncSession = Depends(get_session)):
     return await get_book_by_id(id=id, db=db)
 
 
-@router.get("/")
-async def get_book_with_category(
-    category: str, db: AsyncSession = Depends(get_session)
+@router.post("/add")
+async def add_book(
+    data: BookSchema,
+    token=Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_session),
 ):
-    return await get_book_by_category(category=category, db=db)
+    return await create_book(data=data, token=token, db=db)
