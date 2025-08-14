cfd84c0dcd6719fd9bb99229c5fd7ef90ef7d308
romaperec <cpser621@gmail.com>
2025-08-14 19:32:26 +0300

    delete jwt variables

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -14,8 +14,6 @@ database_config = DatabaseConfig()
 class JWTConfig(BaseSettings):
     SECRET_KEY: str = "p0iy3I05KmNOh8Zjk2eTlVcrccX2VSR-Mfh-BMARw-jV1OpU3f7UDNoyZNae2DyWj0446eeuDi1edHyCPF43PQ"  # It's example
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
